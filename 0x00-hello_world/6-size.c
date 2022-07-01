ae46d6a7b83ee9ce723cea4b45ad387176831c95
P1511 <kamarama.p@gmail.com>
2022-06-30 17:11:06 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 /**
+ *
  *main - Entry point
  * Return: Always 0 (Success)
  */
@@ -10,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-printf("Size of a float: %lu byte(s)\n", sizeof(d));
+printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
