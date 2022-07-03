fe4222f63e70003a3e9c113dee051503c47e0285
P1511 <kamarama.p@gmail.com>
2022-07-03 10:23:49 -0700

    2 print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,15 @@
 *main - the entry point
 *conditions
 *Return: Always 0
-*/
+**/
 int main(void)
 {
-	char a;
-	char A;
-	for (a = 'a'; a <= 'z'; a++)
-		putchar(a);
-	for (A = 'A'; A <= 'Z'; a++)
-		putchar(A);
+	char c;
+	char C;
+	for (c = 'a' ; c <= 'z'; c++)
+		putchar(c);
+	for (C = 'A'; C <= 'Z'; C++
+		putchar(C);
 	putchar('\n');
 	return (0);
 }
