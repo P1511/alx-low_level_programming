a1b905d0d96d1d2f03cb6ff51eac2a1e5b3c6771
P1511 <kamarama.p@gmail.com>
2022-07-03 13:09:07 -0700

    8 print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - entry point
+*print numbers
+*Return: Always 0
+**/
+int main(void)
+{
+int num;
+char letters;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
