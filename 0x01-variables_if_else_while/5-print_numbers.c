d6f41f02bd462e602008fd70fe283b58da35b1d1
P1511 <kamarama.p@gmail.com>
2022-07-03 11:39:44 -0700

    5 print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Print alphabet
+*Return: Always 0
+**/
+int main(void)
+{
+int base_10;
+for (base_10 = 0; base_10 < 10; base_10++)
+{
+printf("%i", base_10);
+}
+printf("\n");
+return (0);
+}
