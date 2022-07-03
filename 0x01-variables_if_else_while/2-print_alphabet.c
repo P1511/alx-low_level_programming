304921051d66091fb5eba41afe930da3a98dca35
P1511 <kamarama.p@gmail.com>
2022-07-03 07:16:25 -0700

    2 print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - expected to print the alphabet
+*print the alphabet
+*Return: Always 0
+*/
+int main(void)
+{
+char a;
+for (a ='a'; a <='z'; a++)
+putchar (a);
+putchar('\n');
+return (0);
+}
