d7f8e63f80e2eb2c80de8bfa05978410f95254e5
P1511 <kamarama.p@gmail.com>
2022-07-03 08:16:55 -0700

    3 print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - the entry point
+*conditions
+*Return: Always 0
+*/
+int main(void)
+{
+char a; 
+for (a = 'a'; a <= 'z' a++)
+putchar(a);
+for ( a = 'A'; a <= 'Z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
