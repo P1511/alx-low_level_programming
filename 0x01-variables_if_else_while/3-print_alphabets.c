fa08994dc78a4abe70ef6ca17979142eb6dc4ca8
P1511 <kamarama.p@gmail.com>
2022-07-03 11:16:50 -0700

    3 prints

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,11 @@
 **/
 int main(void)
 {
-	char c;
-	char C;
-	for (c = 'a' ; c <= 'z'; c++)
-		putchar(c);
-	for (C = 'A'; C <= 'Z'; C++
-		putchar(C);
-	putchar('\n');
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
