ad31b28e5c9d4cdc4c83bfbb92edda48f5a25588
P1511 <kamarama.p@gmail.com>
2022-07-03 13:33:53 -0700

    8 print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-*main - entry point
-*print numbers
-*Return: Always 0
+*main -entry point
+*print all digits
+*Return: 0
 **/
 int main(void)
 {
 int i;
 char x;
 
-for (i = 0; 10 < 10; i++)
+for (i = 0; i < 10; i++)
 putchar(i + '0');
 for (x = 'a'; x <= 'f'; x++)
 putchar(x);
